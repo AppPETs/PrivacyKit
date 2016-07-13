9e4b43e0584e04401de818b2739cd3435a880081
Maximilian Blochberger <9blochbe@informatik.uni-hamburg.de>
2016-07-13 10:55:59 +0200

    Add Xcode project
    Default implementation of Xcode 7.3.1 for Cocoa Touch libraries with
    Swift chosen as a language.

---
+++ PrivacyKit/PrivacyKit.h
@@ -0,0 +1,19 @@
+//
+//  PrivacyKit.h
+//  PrivacyKit
+//
+//  Created by Maximilian Blochberger on 2016-07-13.
+//  Copyright © 2016 Universität Hamburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivacyKit.
+FOUNDATION_EXPORT double PrivacyKitVersionNumber;
+
+//! Project version string for PrivacyKit.
+FOUNDATION_EXPORT const unsigned char PrivacyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivacyKit/PublicHeader.h>
+
+

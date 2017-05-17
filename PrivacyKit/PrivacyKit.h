6c98b2d7c13637d92a12b4b29e33e0e8f273696d
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2017-05-17 09:54:17 +0200

    Simplify supporting file

--- PrivacyKit/PrivacyKit.h
+++ PrivacyKit/PrivacyKit.h
@@ -1,19 +1,7 @@
-//
-//  PrivacyKit.h
-//  PrivacyKit
-//
-//  Created by Maximilian Blochberger on 2016-07-13.
-//  Copyright © 2016 Universität Hamburg. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for PrivacyKit.
 FOUNDATION_EXPORT double PrivacyKitVersionNumber;
 
 //! Project version string for PrivacyKit.
 FOUNDATION_EXPORT const unsigned char PrivacyKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PrivacyKit/PublicHeader.h>
-
-

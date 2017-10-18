d381a7d368d31cf9be3077284cd613efcaa05552
Jakob Rieck <2rieck@informatik.uni-hamburg.de>
2017-10-18 21:19:21 +0200

    Added tests for URLProtocol's parameter extraction from URLs

--- PrivacyKitTests/ShalonTest.swift
+++ PrivacyKitTests/ShalonTest.swift
@@ -34,6 +34,36 @@ class ShalonTest: XCTestCase {
 		}
 	}
 
+	func testShalonURLProtocolParameterExtraction() {
+		// Parsing of a normal URL should result in no parameter extraction
+		XCTAssertNil(try! ShalonURLProtocol.parseShalonParams(from: URL(string: "https://www.google.com")!))
+		XCTAssertNil(try! ShalonURLProtocol.parseShalonParams(from: URL(string: "http://www.example.com")!))
+
+		// Testing incorrectly specified proxies
+		XCTAssertThrowsError(try ShalonURLProtocol.parseShalonParams(from: URL(string: "httpss://shalon1.jondonym.de/www.google.com")!))
+		XCTAssertThrowsError(try ShalonURLProtocol.parseShalonParams(from: URL(string: "httpsss://shalon1.jondonym.de:8080/shalon2.jondonym.de:/www.google.com")!))
+
+		// Testing too few specified proxies
+		XCTAssertThrowsError(try ShalonURLProtocol.parseShalonParams(from: URL(string: "httpss://www.google.com")!))
+		XCTAssertThrowsError(try ShalonURLProtocol.parseShalonParams(from: URL(string: "httpsss://shalon1.jondonym.de:80/www.google.com")!))
+
+		// Testing correct examples
+		// One proxy only
+		let shalonParameters = try! ShalonURLProtocol.parseShalonParams(from: URL(string: "httpss://shalon1.jondonym.de:443/www.google.com")!)
+		XCTAssertNotNil(shalonParameters)
+		XCTAssertEqual(shalonParameters!.proxies.count, 1)
+		XCTAssertEqual(shalonParameters!.proxies[0], Target(withHostname: "shalon1.jondonym.de", andPort: 443))
+		XCTAssertEqual(shalonParameters!.requestUrl, URL(string: "https://www.google.com")!)
+
+		// Two proxies
+		let shalonParameters1 = try! ShalonURLProtocol.parseShalonParams(from: URL(string: "httpsss://shalon1.jondonym.de:443/test.g.de:778/www.google.com")!)
+		XCTAssertNotNil(shalonParameters1)
+		XCTAssertEqual(shalonParameters1!.proxies.count, 2)
+		XCTAssertEqual(shalonParameters1!.proxies[0], Target(withHostname: "shalon1.jondonym.de", andPort: 443))
+		XCTAssertEqual(shalonParameters1!.proxies[1], Target(withHostname: "test.g.de", andPort: 778))
+		XCTAssertEqual(shalonParameters1!.requestUrl, URL(string: "https://www.google.com")!)
+	}
+
 	func testShalonProtocol() {
 		URLProtocol.registerClass(ShalonURLProtocol.self)
 

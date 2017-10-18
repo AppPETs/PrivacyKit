cd04a3c1054884bd0cb7a0a2206f0a1243a212aa
Jakob Rieck <2rieck@informatik.uni-hamburg.de>
2017-10-18 20:48:44 +0200

    Fixed indentation. Simplified URLProtocol code.

--- PrivacyKit/ShalonURLProtocol.swift
+++ PrivacyKit/ShalonURLProtocol.swift
@@ -1,139 +1,135 @@
 import Foundation
 
 class ShalonURLProtocol : URLProtocol {
-    var loadingShouldStop: Bool = false
+	var loadingShouldStop: Bool = false
 
-    struct ShalonProxy {
-        let hostname : String
-        let port : UInt16
-    }
+	struct ShalonParams {
+		let proxies : [Target]
+		let requestUrl : URL
+	}
 
-    struct ShalonParams {
-        let proxies : [ShalonProxy]
-        let requestUrl : URL
-    }
+	class func parseShalonParams(from url: URL) -> ShalonParams? {
+		var numProxies : Int
+		var shalonProxies : [Target] = []
 
-    class func parseShalonParams(from url: URL) -> ShalonParams? {
-        var numProxies : Int
-        var shalonProxies : [ShalonProxy] = []
+		let urlScheme = url.scheme!.lowercased()
+		let sCount = urlScheme.substring(from: urlScheme.range(of: "http")!.upperBound).characters.count
 
-        let urlScheme = url.scheme!.lowercased()
-        let sCount = urlScheme.substring(from: urlScheme.range(of: "http")!.upperBound).characters.count
+		// 1 to 3 proxies (httpss:// to httpssss://) supported
+		guard (2...4).contains(sCount) else {
+			return nil
+		}
 
-        // 1 to 3 proxies (httpss:// to httpssss://) supported
-        guard (2...4).contains(sCount) else {
-            return nil
-        }
+		numProxies = sCount - 1
 
-        numProxies = sCount - 1
+		let urlString = url.absoluteString
+		// The URL string, withouth the scheme part!
+		let baseString = urlString.substring(from: urlString.range(of: "://")!.upperBound)
+		// Examples:
+		//   httpss://proxy:port/destination:port/index.html
+		//   httpsss://proxy1:port/proxy2:port/destination:port/test.txt
+		let components = baseString.components(separatedBy: "/")
+		assert(components.count >= numProxies, "Too few proxies specified!")
 
-        let urlString = url.absoluteString
-        // The URL string, withouth the scheme part!
-        let baseString = urlString.substring(from: urlString.range(of: "://")!.upperBound)
-        // httpss://proxy:port/destination:port/index.html
-        let components = baseString.components(separatedBy: "/")
-        assert(components.count >= numProxies, "Too few proxies specified!")
+		for i in 0..<numProxies {
+			let ithProxy : String = components[i]
+			assert(ithProxy =~ "^[a-zA-Z0-9\\.]*\\:\\d+$", "Incorrect format for proxy specification")
 
-        for i in 0..<numProxies {
-            let ithProxy : String = components[i]
-            assert(ithProxy =~ "^[a-zA-Z0-9\\.]*\\:\\d+$", "Incorrect format for proxy specification")
+			let proxyInfo = ithProxy.components(separatedBy: ":")
+			assert(proxyInfo.count == 2, "Extracted proxy information incomplete")
 
-            let proxyInfo = ithProxy.components(separatedBy: ":")
-            assert(proxyInfo.count == 2, "Extracted proxy information incomplete")
+			let shalonProxy = Target(withHostname: proxyInfo[0], andPort: UInt16(proxyInfo[1])!)
+			shalonProxies.append(shalonProxy!)
+		}
 
-            let shalonProxy = ShalonProxy(hostname: proxyInfo[0], port: UInt16(proxyInfo[1])!)
-            shalonProxies.append(shalonProxy)
-        }
+		// The requestUrl is now simply those components that were not used before,
+		// along with the https:// scheme
+		let requestUrl = URL(string: "https://" + components[numProxies..<components.count].joined(separator: "/"))
+		if let actualUrl = requestUrl {
+			return ShalonParams(proxies: shalonProxies, requestUrl: actualUrl)
+		}
 
-        // The requestUrl is now simply those components that were not used before,
-        // along with the https:// scheme
-        let requestUrl = URL(string: "https://" + components[numProxies..<components.count].joined(separator: "/"))
-        if let actualUrl = requestUrl {
-            print(shalonProxies)
-            print(actualUrl)
-            return ShalonParams(proxies: shalonProxies, requestUrl: actualUrl)
-        }
+		return nil
+	}
 
-        return nil
-    }
+	override class func canInit(with request: URLRequest) -> Bool {
+		if let url = request.url {
+			let shalonParameters = parseShalonParams(from: url)
+			if shalonParameters != nil {
+				print("Shalon will handle request")
+				return true
+			} else {
+				print("Shalon will not handle request")
+				return false
+			}
+		}
 
-    override class func canInit(with request: URLRequest) -> Bool {
-        if let url = request.url {
-            let shalonParameters = parseShalonParams(from: url)
-            if shalonParameters != nil {
-                print("Shalon will handle request")
-                return true
-            } else {
-                print("Shalon will not handle request")
-                return false
-            }
-        }
+		return false
+	}
 
-        return false
-    }
+	override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+		return request
+	}
 
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-        return request
-    }
+	enum ShalonErrors : Error {
+		case NotImplemented
+		case UnknownHTTPMethod
+		case NoContents
+	}
 
-    enum ShalonErrors : Error {
-        case NotImplemented
-        case UnknownHTTPMethod
-        case NoContents
-    }
+	override func startLoading() {
+		// ShalonParameters is only ever called if canInit returned true,
+		// meaning that this function successfully executed and does not
+		// return nil
+		let shalonParameters = ShalonURLProtocol.parseShalonParams(from: request.url!)!
 
-    override func startLoading() {
-        let shalonParameters = ShalonURLProtocol.parseShalonParams(from: request.url!)
-        assert(shalonParameters != nil)
+		print(shalonParameters.requestUrl)
 
-        print(shalonParameters!.requestUrl)
+		let target = Target(withHostname: shalonParameters.requestUrl.host!, andPort: 443)!
+		let shalon = Shalon(withTarget: target)
 
-        let target = Target(withHostname: shalonParameters!.requestUrl.host!, andPort: 443)!
-        let shalon = Shalon(withTarget: target)
+		for proxy in shalonParameters.proxies {
+			shalon.addLayer(proxy)
+		}
 
-        // Add Shalon layers from parameters
-        for proxy in shalonParameters!.proxies {
-            shalon.addLayer(Target(withHostname: proxy.hostname, andPort: proxy.port)!)
-        }
+		let optionalMethod = Method(rawValue: request.httpMethod!.uppercased())
+		guard optionalMethod != nil else {
+			client!.urlProtocol(self, didFailWithError: ShalonErrors.UnknownHTTPMethod)
+			return
+		}
 
-        let optionalMethod = Method(rawValue: request.httpMethod!.uppercased())
-        guard optionalMethod != nil else {
-            client!.urlProtocol(self, didFailWithError: ShalonErrors.UnknownHTTPMethod)
-            return
-        }
+		shalon.issue(request: Request(withMethod: optionalMethod!, andUrl: shalonParameters.requestUrl)!) {
+			receivedOptionalResponse, receivedOptionalError in
 
-        shalon.issue(request: Request(withMethod: optionalMethod!, andUrl: shalonParameters!.requestUrl)!) {
-            receivedOptionalResponse, receivedOptionalError in
+			print("Handling response from URLProtocol handler")
+			guard !self.loadingShouldStop else {
+				return
+			}
 
-            print("Handling response from URLProtocol handler")
-            guard !self.loadingShouldStop else {
-                return
-            }
+			// Handle any errors
+			if let error = receivedOptionalError {
+				self.client!.urlProtocol(self, didFailWithError: error)
+				return
+			}
 
-            // Handle any errors
-            if let error = receivedOptionalError {
-                self.client!.urlProtocol(self, didFailWithError: error)
-                return
-            }
+			// Handle a correct response
+			if let response = receivedOptionalResponse {
+				// Convert internal response type to URLResponse
+				let url_response = HTTPURLResponse(url: self.request.url!,
+												   statusCode: Int(response.status.rawValue),
+												   httpVersion: "HTTP/1.1",
+												   headerFields: response.headers)
+				self.client!.urlProtocol(self, didReceive: url_response!, cacheStoragePolicy: .allowed)
+				self.client!.urlProtocol(self, didLoad: response.body)
+				self.client!.urlProtocolDidFinishLoading(self)
+				return
+			}
 
-            // Handle a correct response
-            if let response = receivedOptionalResponse {
-                // Convert internal response type to URLResponse
-                let url_response = HTTPURLResponse(url: self.request.url!,
-                                                   statusCode: Int(response.status.rawValue),
-                                                   httpVersion: "HTTP/1.1",
-                                                   headerFields: response.headers)
-                self.client!.urlProtocol(self, didReceive: url_response!, cacheStoragePolicy: .allowed)
-                self.client!.urlProtocol(self, didLoad: response.body)
-                self.client!.urlProtocolDidFinishLoading(self)
-                return
-            }
+			self.client!.urlProtocol(self, didFailWithError: ShalonErrors.NoContents)
+		}
+	}
 
-            self.client!.urlProtocol(self, didFailWithError: ShalonErrors.NoContents)
-        }
-    }
-
-    override func stopLoading() {
-        loadingShouldStop = true
-    }
+	override func stopLoading() {
+		loadingShouldStop = true
+	}
 }
--- PrivacyKitTests/ShalonTest.swift
+++ PrivacyKitTests/ShalonTest.swift
@@ -3,59 +3,59 @@ import XCTest
 
 class ShalonTest: XCTestCase {
 
-    func testExample() {
-        let url = URL(string: "https://example.com/")!
-        let target = Target(withHostname: url.host!, andPort: 443)!
-        let shalon = Shalon(withTarget: target)
+	func testExample() {
+		let url = URL(string: "https://example.com/")!
+		let target = Target(withHostname: url.host!, andPort: 443)!
+		let shalon = Shalon(withTarget: target)
 
-        var optionalResponse: Response? = nil
-        var optionalError: Error? = nil
+		var optionalResponse: Response? = nil
+		var optionalError: Error? = nil
 
-        shalon.addLayer(Target(withHostname: "shalon1.jondonym.net", andPort: 443)!)
+		shalon.addLayer(Target(withHostname: "shalon1.jondonym.net", andPort: 443)!)
 
-        let responseReceivedExpectation = expectation(description: "responseReceived")
+		let responseReceivedExpectation = expectation(description: "responseReceived")
 
-        shalon.issue(request: Request(withMethod: .head, andUrl: url)!) {
-            receivedOptionalResponse, receivedOptionalError in
+		shalon.issue(request: Request(withMethod: .head, andUrl: url)!) {
+			receivedOptionalResponse, receivedOptionalError in
 
-            optionalResponse = receivedOptionalResponse
-            optionalError = receivedOptionalError
+			optionalResponse = receivedOptionalResponse
+			optionalError = receivedOptionalError
 
-            responseReceivedExpectation.fulfill()
-        }
+			responseReceivedExpectation.fulfill()
+		}
 
-        waitForExpectations(timeout: 10/*seconds*/) {
-            optionalExpectationError in
+		waitForExpectations(timeout: 10/*seconds*/) {
+			optionalExpectationError in
 
-            XCTAssertNil(optionalExpectationError, "Expectation handled erroneously")
+			XCTAssertNil(optionalExpectationError, "Expectation handled erroneously")
 
-            XCTAssertNotNil(optionalResponse)
-            XCTAssertNil(optionalError)
-        }
-    }
+			XCTAssertNotNil(optionalResponse)
+			XCTAssertNil(optionalError)
+		}
+	}
 
-    func testShalonProtocol() {
-        URLProtocol.registerClass(ShalonURLProtocol.self)
+	func testShalonProtocol() {
+		URLProtocol.registerClass(ShalonURLProtocol.self)
 
-        // Shalon Test
-        let shalonReceivedExpectation = expectation(description: "shalonResponseReceived")
+		// Shalon Test
+		let shalonReceivedExpectation = expectation(description: "shalonResponseReceived")
 
-        var shalonResponse: URLResponse? = nil
+		var shalonResponse: URLResponse? = nil
 
-        let sharedSession = URLSession.shared
-        let shalonTask = sharedSession.downloadTask(with: URL(string: "httpss://shalon1.jondonym.net:443/example.com/")!) {
-            (potentialUrl, potentialResponse, potentialError) in
+		let sharedSession = URLSession.shared
+		let shalonTask = sharedSession.downloadTask(with: URL(string: "httpss://shalon1.jondonym.net:443/example.com/")!) {
+			(potentialUrl, potentialResponse, potentialError) in
 
-            shalonResponse = potentialResponse
-            shalonReceivedExpectation.fulfill()
-        }
-        shalonTask.resume()
+			shalonResponse = potentialResponse
+			shalonReceivedExpectation.fulfill()
+		}
+		shalonTask.resume()
 
-        waitForExpectations(timeout: 10/*seconds*/) {
-            optionalExpectationError in
+		waitForExpectations(timeout: 10/*seconds*/) {
+			optionalExpectationError in
 
-            XCTAssertNil(optionalExpectationError, "Expectation handled erroneously")
-            XCTAssertNotNil(shalonResponse)
-        }
-    }
+			XCTAssertNil(optionalExpectationError, "Expectation handled erroneously")
+			XCTAssertNotNil(shalonResponse)
+		}
+	}
 }

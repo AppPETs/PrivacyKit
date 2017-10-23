ac0c2f71ddc9e8522a15f1e36221b3b7a33a75e3
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2017-10-23 09:58:58 +0200

    Fix IPv6 test

--- PrivacyKitTests/ShalonTest.swift
+++ PrivacyKitTests/ShalonTest.swift
@@ -71,7 +71,7 @@ class ShalonTest: XCTestCase {
 		XCTAssertEqual(ipv6Params!.requestUrl, URL(string: "https://www.google.com")!)
 
 		// Testing incorrectly specified IPv6 address
-		XCTAssertThrowsError(try! ShalonURLProtocol.parseShalonParams(from: URL(string: "httpss://2001:db8:85a3::8a2e:370:7334:443/www.google.com")!))
+		XCTAssertThrowsError(try ShalonURLProtocol.parseShalonParams(from: URL(string: "httpss://2001:db8:85a3::8a2e:370:7334:443/www.google.com")!))
 	}
 
 	func testShalonProtocol() {

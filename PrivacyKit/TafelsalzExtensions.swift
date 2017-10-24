9b6ff201a598ea619d6b3795760146bf9232ed1a
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2017-10-24 13:31:21 +0200

    Export master keys Base64 encoded
    Some QR code readers only support strings and not data. In addition
    Base64 encoded data could also be imported without relying on QR code
    scanning.

--- PrivacyKit/TafelsalzExtensions.swift
+++ PrivacyKit/TafelsalzExtensions.swift
@@ -3,6 +3,18 @@ import Tafelsalz
 
 extension MasterKey {
 	public func qrCode() -> QRCode? {
-		return QRCode(copyBytes())
+		return QRCode(base64EncodedString())
+	}
+
+	public func base64EncodedString() -> String {
+		return copyBytes().base64EncodedString()
+	}
+
+	public convenience init?(base64Encoded encodedString: String) {
+		guard var data = Data(base64Encoded: encodedString) else {
+			return nil
+		}
+
+		self.init(bytes: &data)
 	}
 }
--- PrivacyKitTests/TafelsalzExtensionsTest.swift
+++ PrivacyKitTests/TafelsalzExtensionsTest.swift
@@ -10,7 +10,21 @@ class TafelsalzExtensionsTest: XCTestCase {
 		let masterKey = MasterKey()
 		let qr = masterKey.qrCode()!
 
-		XCTAssertEqual(qr.data, masterKey.copyBytes())
+		XCTAssertEqual(String(bytes: qr.data, encoding: .utf8)!, masterKey.base64EncodedString())
+	}
+
+	func testMasterKeyFromBase64Encoded() {
+		let masterKey1 = MasterKey()
+		let masterKey2 = MasterKey(base64Encoded: masterKey1.base64EncodedString())!
+
+		XCTAssertEqual(masterKey2.copyBytes(), masterKey1.copyBytes())
+		XCTAssertEqual(masterKey2.base64EncodedString(), masterKey1.base64EncodedString())
+
+		let invalidLength = String(masterKey1.base64EncodedString()[String.Index(encodedOffset: 1)...])
+		XCTAssertNil(MasterKey(base64Encoded: invalidLength))
+
+		let invalidCharacter = ":" + masterKey1.base64EncodedString().dropFirst()
+		XCTAssertNil(MasterKey(base64Encoded: invalidCharacter))
 	}
 
 }

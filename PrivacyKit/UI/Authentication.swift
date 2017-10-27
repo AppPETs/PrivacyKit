c7386b840eb450e4b7db877edec2117deec7a7ac
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2017-10-27 23:04:51 +0200

    Add UI components
    First, activity indicators have been moved into a subfolder. Second a
    class for authenticating the device owner has been added and third an
    image view class was added, that can be used to display a secret key or
    similar for sharing with another device.

---
+++ PrivacyKit/UI/Authentication.swift
@@ -0,0 +1,117 @@
+import LocalAuthentication
+
+/**
+	A context for which the authentication is valid.
+
+	The context should be invalidated by calling `invalidate()` as soon as it is
+	no longer required. All outstanding authentications for this context will be
+	cancelled.
+*/
+public typealias AuthenticationContext = LAContext
+
+/**
+	Errors that might arise during authentication.
+*/
+public enum AuthenticationError: Error {
+
+	/**
+		The user did not enter the correct credentials.
+	*/
+	case authenticationFailed
+
+	/**
+		The authentication context is invalid.
+	*/
+	case invalidContext
+
+	/**
+		There is no passcode set for the device and therefore the authentication
+		mechanism is not present. A custom authentication function could be
+		offered, e.g., by storing a password by using `HashedPassword`.
+	*/
+	case passcodeNotSet
+
+	/**
+		The OS cancelled the authentication, e.g., because another app moved
+		into the foreground. Displaying an error message in this case might not
+		be neccessary, see if it can be handled like `.userCancel`.
+	*/
+	case systemCancel
+
+	/**
+		The user cancelled the authentication. Avoid displaying error messages
+		in this case and try to handle this gracefully.
+	*/
+	case userCancel
+
+	/**
+		The user was not able to authenticate via Touch ID or Face ID for three
+		subsequent attempts.
+	*/
+	case tooManyFailedAttempts
+}
+
+/**
+	Challenges the user to authenticate as the device owner. Biometric
+	authentication (Face ID or Touch ID) is tried first if available and
+	passcode authentication is used as a fallback.
+
+	#### Example
+	```swift
+	context = authenticateDeviceOwner(reason: "Unlock something") {
+	    authenticationError in
+
+	    guard authenticationError == nil else {
+	        // Failed to authenticate (the user just might have cancelled)
+	        // TODO: Handle error
+	        return
+	    }
+
+	    // Successfully authenticated
+	    unlockSomething()
+	}
+	```
+
+	- note:
+		In order to use Face ID, add `NSFaceIDUsageDescription` to your
+		`Info.plist`.
+
+	- parameters:
+		- reason: The reason describing the purpose of the authentication.
+		- completion: A function that is called after the authentication
+			finished (not neccessarily successfully).
+		- authenticationError: Upon successful authentication this is `nil` else
+			it contains the cause for failure.
+
+	- returns:
+		The authentication context.
+*/
+public func authenticateDeviceOwner(reason: String, completion: @escaping (_ authenticationError: AuthenticationError?) -> Void) -> AuthenticationContext {
+	let context = AuthenticationContext()
+
+	context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) {
+		success, error in
+
+		guard success else {
+			switch error! {
+				case LAError.authenticationFailed: completion(.authenticationFailed)
+				case LAError.appCancel:            fatalError() // When does this happen?
+				case LAError.invalidContext:       completion(.invalidContext)
+				case LAError.notInteractive:       fatalError() // When does this happen?
+				case LAError.passcodeNotSet:       completion(.passcodeNotSet)
+				case LAError.systemCancel:         completion(.systemCancel)
+				case LAError.userCancel:           completion(.userCancel)
+				case LAError.biometryLockout:      completion(.tooManyFailedAttempts)
+				case LAError.userFallback:         fatalError() // No fallback, policy falls back automatically
+				case LAError.biometryNotEnrolled:  fatalError() // Policy falls back automatically
+				case LAError.biometryNotAvailable: fatalError() // Policy falls back automatically
+				default:                           fatalError("UNREACHABLE")
+			}
+			return
+		}
+
+		completion(nil)
+	}
+
+	return context
+}
---
+++ PrivacyKit/UI/ConfidentialQrCodeView.swift
@@ -0,0 +1,109 @@
+#if os(iOS)
+	import UIKit
+
+	public typealias Image = UIImage
+	public typealias ImageView = UIImageView
+#endif // iOS
+
+#if os(macOS)
+	import Cocoa
+
+	public typealias Image = NSImage
+	public typealias ImageView = NSImageView
+#endif
+
+import QRCode
+
+/**
+	This class can be used to share secrets with other devices. A cover image,
+	that can be set using Interface builder will be displayed and the QR code
+	will only be uncovered if the user taps or clicks on the image view and
+	has authenticated himself.
+*/
+public class ConfidentialQrCodeView: ImageView {
+
+	private var coverImage: Image? = nil
+
+	private var context: AuthenticationContext? = nil
+	private var isConfidentialValueDisplayed = false
+	public var qrCode: QRCode! = nil
+
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+
+		coverImage = image
+
+		#if os(iOS)
+			isUserInteractionEnabled = true
+
+			let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleDisplayingConfidentialValue))
+			tapGestureRecognizer.numberOfTapsRequired = 1
+			tapGestureRecognizer.numberOfTouchesRequired = 1
+			addGestureRecognizer(tapGestureRecognizer)
+		#endif // iOS
+
+		#if os(macOS)
+			// <#TODO#> Untested.
+			self.action = #selector(toggleDisplayingConfidentialValue)
+			self.target = self
+		#endif
+	}
+
+	private func uncover() {
+		DispatchQueue.main.async {
+			self.image = self.qrCode.image
+			self.isConfidentialValueDisplayed = true
+		}
+	}
+
+	private func showError(message: String) {
+		let title = "Authentication failed"
+
+		#if os(iOS)
+			let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "Ok", style: .default))
+			UIApplication.shared.keyWindow!.rootViewController!.present(alert, animated: true)
+		#endif // iOS
+
+		#if os(macOS)
+			// <#TODO#> Untested.
+			let alert = NSAlert()
+			alert.addButton(withTitle: "Ok")
+			alert.messageText = title
+			alert.informativeText = message
+			alert.alertStyle = .warning
+			alert.beginSheetModal(for: NSApp.mainWindow!)
+		#endif // macOS
+	}
+
+	@objc
+	private func toggleDisplayingConfidentialValue() {
+		if isConfidentialValueDisplayed {
+			// Show cover image
+			context?.invalidate()
+			image = coverImage
+			isConfidentialValueDisplayed = false
+		} else {
+			// Authenticate the user with FaceID or TouchID, if these methods
+			// are available
+			let reason = "You need to authenticate, in order to show the secret key."
+			context = authenticateDeviceOwner(reason: reason) {
+				authenticationError in
+
+				// <#FIXME#> Why does an .invalidContext error sometimes occur here?
+				guard authenticationError == nil else {
+					// Ignored errors do not uncover the QR code.
+					let ignoredAuthenticationErrors: [AuthenticationError] = [.systemCancel, .userCancel]
+					if !ignoredAuthenticationErrors.contains(authenticationError!) {
+						self.showError(message: authenticationError!.localizedDescription)
+					}
+
+					return
+				}
+
+				self.uncover()
+			}
+		}
+	}
+
+}

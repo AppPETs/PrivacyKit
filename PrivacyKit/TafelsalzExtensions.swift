d2c3a30a61a7be7d989da1a5e93a8a164c49e2a0
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2018-08-17 13:59:18 +0200

    Minor code simplification

--- PrivacyKit/TafelsalzExtensions.swift
+++ PrivacyKit/TafelsalzExtensions.swift
@@ -3,7 +3,7 @@ import Tafelsalz
 
 // <#FIXME#> Remove Android workaround (Todo-iOS/#2)
 let MASTER_KEY_PREFIX = "PLIB"
-let MASTER_KEY_PREFIX_SIZE_IN_BYTES = Data(MASTER_KEY_PREFIX.utf8).count
+let MASTER_KEY_PREFIX_SIZE_IN_BYTES = MASTER_KEY_PREFIX.utf8Bytes.count
 
 extension MasterKey {
 

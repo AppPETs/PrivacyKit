ed9e00c7c229d5d4ced77df7762e07408e5d8185
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2018-08-18 22:51:30 +0200

    Add documentation for HTTP enums

--- PrivacyKit/Http.swift
+++ PrivacyKit/Http.swift
@@ -29,13 +29,21 @@ public class Http {
 		A type-safe HTTP method.
 	*/
 	public enum Method: String {
+		/// CONNECT
 		case connect = "CONNECT"
+		/// DELETE
 		case delete  = "DELETE"
+		/// GET
 		case get     = "GET"
+		/// HEAD
 		case head    = "HEAD"
+		/// OPTIONS
 		case options = "OPTIONS"
+		/// POST
 		case post    = "POST"
+		/// PUT
 		case put     = "PUT"
+		/// TRACE
 		case trace   = "TRACE"
 	}
 
@@ -43,10 +51,15 @@ public class Http {
 		A type-safe HTTP status category.
 	*/
 	public enum StatusCategory {
+		/// Informal response (1xx)
 		case informal
+		/// Success (2xx)
 		case success
+		/// Redirection (3xx)
 		case redirection
+		/// Client errors (4xx)
 		case clientError
+		/// Server errors (5xx)
 		case serverError
 	}
 
@@ -54,71 +67,142 @@ public class Http {
 		A type-safe HTTP status.
 	*/
 	public enum Status: UInt16 {
-		// Informal
+
+		// Informal response
+
+		/// Continue
 		case Continue                      = 100
+		/// Swithing protocols
 		case switchingProtocols            = 101
+		/// Processing
 		case processing                    = 102
+
 		// Success
+
+		/// OK
 		case ok                            = 200
+		/// Created
 		case created                       = 201
+		/// Accepted
 		case accepted                      = 202
+		/// Non-authoritative information
 		case nonAuthoritativeInformation   = 203
+		/// No content
 		case noContent                     = 204
+		/// Reset content
 		case resetContent                  = 205
+		/// Partial content
 		case partialContent                = 206
+		/// Multi-status
 		case multiStatus                   = 207
+		/// Already reported
 		case alreadyReported               = 208
+		/// IM used
 		case imUsed                        = 226
+
 		// Redirection
+
+		/// Multiple choices
 		case multipleChoices               = 300
+		/// Moved permanently
 		case movedPermanently              = 301
+		/// Found
 		case found                         = 302
+		/// See other
 		case seeOther                      = 303
+		/// Not modified
 		case notModified                   = 304
+		/// Use proxy
 		case useProxy                      = 305
+		/// Switch proxy
 		case switchProxy                   = 306
+		/// Temporary redirect
 		case temporaryRedirect             = 307
+		/// Permanent redirect
 		case permanentRedirect             = 308
+
 		// Client errors
+
+		/// Bad request
 		case badRequest                    = 400
+		/// Unauthorized
 		case unauthorized                  = 401
+		/// Payment required
 		case paymentRequired               = 402
+		/// Forbidden
 		case forbidden                     = 403
+		/// Not found
 		case notFound                      = 404
+		/// Method not allowed
 		case methodNotAllowed              = 405
+		/// Not acceptable
 		case notAcceptable                 = 406
+		/// Proxy authentication required
 		case proxyAuthenticationRequired   = 407
+		/// Request timeout
 		case requestTimeout                = 408
+		/// Conflict
 		case conflict                      = 409
+		/// Gone
 		case gone                          = 410
+		/// Length required
 		case lengthRequired                = 411
+		/// Precodition failed
 		case preconditionFailed            = 412
+		/// Payload too large
 		case payloadTooLarge               = 413
+		/// URI too long
 		case uriTooLong                    = 414
+		/// Unsupported media type
 		case unsupportedMediaType          = 415
+		/// Range not satisfiable
 		case rangeNotSatisfiable           = 416
+		/// Expectation failed
 		case expectationFailed             = 417
+		/// Teapot
 		case teapot                        = 418
+		/// Misdirected request
 		case misdirectedRequest            = 421
+		/// Unprocessable entity
 		case unprocessableEntity           = 422
+		/// Locked
 		case locked                        = 423
+		/// Failed dependency
 		case failedDependency              = 424
+		/// Upgrade required
 		case upgradeRequired               = 426
+		/// Precondition required
 		case preconditionRequired          = 428
+		/// Too many requests
 		case tooManyRequests               = 429
+		/// Request header fields too large
 		case requestHeaderFieldsTooLarge   = 431
+		/// Unavailable for legal reasons
 		case unavailableForLegalReasons    = 451
+
 		// Server Errors
+
+		/// Internal server error
 		case internalServerError           = 500
+		/// Not implemented
 		case notImplemented                = 501
+		/// Bad gateway
 		case badGateway                    = 502
+		/// Service unavailable
 		case serviceUnavailable            = 503
+		/// Gateway timeout
 		case gatewayTimeout                = 504
+		/// HTTP version not supported
 		case httpVersionNotSupported       = 505
+		/// Variant also negotiates
 		case variantAlsoNegotiates         = 506
+		/// Insufficient storage
 		case insufficientStorage           = 507
+		/// Loop detected
 		case loopDetected                  = 508
+		/// Not extended
 		case notExtended                   = 510
+		/// Network authentication required
 		case networkAuthenticationRequired = 511
 
 		/**
